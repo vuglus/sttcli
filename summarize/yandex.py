93524268f929c4aca4f9ad9a32f9d58a774a2fc3
Alexey Vinogradov <avinogradov@gmail.com>
2025-11-18 17:19:31 +0300

    [*] add instruction select

--- summarize/yandex.py
+++ summarize/yandex.py
@@ -30,43 +30,7 @@ def chunk_summarize(config, text, instruction):
         temperature=0.3,
         instructions = instruction,
         input=text,
-        max_output_tokens=500
-    )
-    # Ловим явные ошибки
-    if response.error:
-        raise RuntimeError(f"YandexGPT error: {response.error}")
-
-    # Ловим ситуацию когда токены переполнились и ответа нет
-    if not response.output_text:
-        raise RuntimeError("Empty YandexGPT response (probably input token overflow)")    
-
-    return response.output_text
-
-
-#
-def instructions_summarize(config, text, instructions):
-    client = openai.OpenAI(
-        api_key=config["yacloud"]["api_key"],
-        base_url="https://rest-assistant.api.cloud.yandex.net/v1",
-        project=config["yacloud"]["folder_id"]
-    )
-    response = client.responses.create(
-        model=f"gpt://{config['yacloud']['folder_id']}/yandexgpt/rc",
-        temperature=0.3,
-        instructions=tpl(
-            "Ты помощник, делающий краткое саммари текста.\n"
-            "Важно не что обсуждали, а какие решения приняли и о каких задачах договорились.\n"
-            "Кто эти задачи и когда договорились сделать.\n"
-            "Обрати внимание именно на изменения — т.е. начинали обсуждать А, но потом решили отказаться и сделать Б.\n"
-            "Формат вывода — MD файл.\n"
-            "Добавь заголовок.\n"
-            "си пи эс - это CPS customer profile service, "
-            "монолит - это система управления заказами и интеграционный шлюз,"
-            "Список задач, если о них договорились, оформи таблицей: задача | кто делает.\n"
-            "Отдельной строкой добавь название файла с краткой сутью встречи."
-        ),
-        input=text,
-        max_output_tokens=500
+        max_output_tokens=config['yacloud']['max_output']
     )
     # Ловим явные ошибки
     if response.error:
